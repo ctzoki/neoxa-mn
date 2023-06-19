d5e934d9541b804153f807e51cbd7de695e5d076
ctzoki <ctzoki@protonmail.com>
2023-06-19 22:31:54 +0400

    fix again bad url

--- install-smartnode-status.sh
+++ install-smartnode-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the GitHub repository URL
-REPO_URL="https://github.com/ctzoki/neoxa-mn"
+REPO_URL="https://raw.githubusercontent.com/ctzoki/neoxa-mn"
 
 # Define the target directory
 TARGET_DIR="/usr/share/cockpit/smartnode-status"
@@ -15,11 +15,14 @@ fi
 cd "$TARGET_DIR" || exit 1
 
 # Download and overwrite the files from the "smartnode-status" folder in the GitHub repository
-FOLDER_URL="$REPO_URL/raw/main/smartnode-status"
+FOLDER_URL="$REPO_URL/main/smartnode-status"
 files=("bootstrap.bundle.min.js" "bootstrap.min.css" "custom.css" "manifest.json" "smartnode-status.html" "smartnode-status.sh")
 for file in "${files[@]}"; do
     wget -q --no-clobber "$FOLDER_URL/$file"
 done
+
+# Make the smartnode-status.sh file executable
+chmod +x smartnode-status.sh
 
 # Add cron job to run the script every minute if it doesn't exist
 CRON_CMD="cd $TARGET_DIR && bash smartnode-status.sh"
