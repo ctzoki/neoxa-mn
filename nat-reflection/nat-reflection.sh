7d1190591a7277f13b0503082807e17d1c309465
ctzoki <129646348+ctzoki@users.noreply.github.com>
2023-08-06 10:16:02 +0200

    Update nat-reflection.sh
    fix for rules not reloading on boot, added a 3 second delay on boot as a workaround until better solution

--- nat-reflection/nat-reflection.sh
+++ nat-reflection/nat-reflection.sh
@@ -5,41 +5,41 @@ RULES_PATH="/etc/nat-reflection"
 
 # Check if script is run as root
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit
+  echo "Error: Please run as root"
+  exit 1
 fi
 
 # Create rules directory if not exist
-mkdir -p $RULES_PATH
+mkdir -p $RULES_PATH || { echo 'Error: Failed to create rules directory.'; exit 1; }
 
 function add_rule() {
   local ip=$1
   local dest=$2
-  iptables -t nat -A PREROUTING -d $ip -j DNAT --to-destination $dest
-  echo "$ip $dest" >> "$RULES_PATH/rules"
+  iptables -t nat -A PREROUTING -d $ip -j DNAT --to-destination $dest || { echo "Error: Failed to add rule."; exit 1; }
+  echo "$ip $dest" >> "$RULES_PATH/rules" || { echo 'Error: Failed to write rule to file.'; exit 1; }
+  echo "Success: Rule added."
 }
 
 function list_rules() {
-  cat "$RULES_PATH/rules"
+  cat "$RULES_PATH/rules" || { echo 'Error: Failed to list rules.'; exit 1; }
 }
 
 function delete_rule() {
   local ip=$1
   local dest=$2
-  if iptables -t nat -C PREROUTING -d $ip -j DNAT --to-destination $dest >/dev/null 2>&1; then
-    iptables -t nat -D PREROUTING -d $ip -j DNAT --to-destination $dest
-  fi
-  sed -i "/$ip $dest/d" "$RULES_PATH/rules"
+  iptables -t nat -D PREROUTING -d $ip -j DNAT --to-destination $dest || { echo 'Error: Failed to delete rule.'; exit 1; }
+  sed -i "/$ip $dest/d" "$RULES_PATH/rules" || { echo 'Error: Failed to delete rule from file.'; exit 1; }
+  echo "Success: Rule deleted."
 }
 
 function reload_rules() {
+  sleep 3
   while IFS=' ' read -r line; do
     IFS=' ' read -r ip dest <<<"$line"
-    if iptables -t nat -C PREROUTING -d $ip -j DNAT --to-destination $dest >/dev/null 2>&1; then
-      iptables -t nat -D PREROUTING -d $ip -j DNAT --to-destination $dest
-    fi
-    iptables -t nat -A PREROUTING -d $ip -j DNAT --to-destination $dest
+    iptables -t nat -D PREROUTING -d $ip -j DNAT --to-destination $dest >/dev/null 2>&1
+    iptables -t nat -A PREROUTING -d $ip -j DNAT --to-destination $dest || { echo "Error: Failed to reload rule."; exit 1; }
   done < "$RULES_PATH/rules"
+  echo "Success: Rules reloaded."
 }
 
 case "$1" in
